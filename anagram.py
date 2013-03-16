ce441d910d0d00c11b25931f8f78384f77525477
Chris Moultrie <cmoultrie@endgames.us>
2013-03-15 21:50:54 -0700

    Removed extranous set

--- anagram.py
+++ anagram.py
@@ -12,17 +12,15 @@ def build_word_list(text):
 def find_anagram(word_list):
     combined_words = itertools.combinations(word_list, 2)
 
-    unique_list = set()
     unique_sets = {}
 
     for word in combined_words:
         joined = ''.join(sorted("%s%s" % (word[0], word[1])))
-        if joined in unique_list:
+        if joined in unique_sets:
             other = unique_sets[joined]
             if not word[0] in other and not word[1] in other:
                 print("%s %s, %s %s" % (other[0], other[1], word[0], word[1]))
         else:
-            unique_list.add(joined)
             unique_sets[joined] = tuple([word[0], word[1]])
 
 if __name__ == '__main__':

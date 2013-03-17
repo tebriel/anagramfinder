fb8d0607bd497664cd96d3db337e0d6ef70ac622
Chris Moultrie <cmoultrie@endgames.us>
2013-03-17 15:09:38 -0700

    Much faster (esp with pypy)

--- anagram2.py
+++ anagram2.py
@@ -5,19 +5,19 @@ import itertools
 import sys
 import time
 
-class UniqueThing:
+class UniqueThing(object):
+    __slots__ = ['item_hash', 'item_tuples']
     def __init__(self):
         self.item_hash = {}
         self.item_tuples = []
-    def add_tuple(self, word_tuple):
-        if not self.item_hash.get(word_tuple[0]) and not self.item_hash.get(word_tuple[1]):
-            self.item_hash[word_tuple[0]] = True
-            self.item_hash[word_tuple[1]] = True
-            self.item_tuples.append(word_tuple)
+    def add_tuple(self, word_one, word_two):
+        if not self.item_hash.get(word_one) and not self.item_hash.get(word_two):
+            self.item_hash[word_one] = True
+            self.item_hash[word_two] = True
+            self.item_tuples.append(tuple([word_one, word_two]))
 
 def build_word_list(text):
-    word_reg = re.compile('[a-zA-Z]{4,}')
-    return set([x.lower() for x in re.findall(word_reg, text)])
+    return set(re.findall('[a-z]{4,}', text.lower()))
 
 def print_groups(groups, count):
     for key in groups:
@@ -32,17 +32,20 @@ def find_anagram(word_list):
 
     unique_groups = {}
 
-    for word in combined_words:
-        joined = ''.join(sorted("%s%s" % (word[0], word[1])))
-        if not joined in unique_groups:
+    for word_one, word_two in combined_words:
+        joined = ''.join(sorted("%s%s" % (word_one, word_two)))
+        if not unique_groups.get(joined):
             unique_groups[joined] = UniqueThing()
-        unique_groups[joined].add_tuple(tuple([word[0], word[1]]))
+        unique_groups[joined].add_tuple(word_one, word_two)
 
     return unique_groups
 
 if __name__ == '__main__':
+    text = sys.stdin.read()
     start_time = time.time()
-    word_list = build_word_list(sys.stdin.read())
+    word_list = build_word_list(text)
+    print(time.time() - start_time, "seconds")
     groups = find_anagram(word_list)
+    print(time.time() - start_time, "seconds")
     print_groups(groups, 10)
     print(time.time() - start_time, "seconds")

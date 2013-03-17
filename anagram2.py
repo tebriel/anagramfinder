f5fde0ba08cea1d422fec3f8bebcb6f6a4f9be80
Chris Moultrie <cmoultrie@endgames.us>
2013-03-17 15:53:57 -0700

    Current impl is 14.27s in pypy

--- anagram2.py
+++ anagram2.py
@@ -8,24 +8,20 @@ import time
 class UniqueThing(object):
     __slots__ = ['item_hash', 'item_tuples']
     def __init__(self):
-        self.item_hash = {}
+        self.item_hash = []
         self.item_tuples = []
     def add_tuple(self, word_one, word_two):
-        if not self.item_hash.get(word_one) and not self.item_hash.get(word_two):
-            self.item_hash[word_one] = True
-            self.item_hash[word_two] = True
-            self.item_tuples.append(tuple([word_one, word_two]))
+        if not word_one in self.item_hash and not word_two in self.item_hash:
+            self.item_hash.extend([word_one, word_two])
+            self.item_tuples.append([word_one, word_two])
 
 def build_word_list(text):
     return set(re.findall('[a-z]{4,}', text.lower()))
 
-def print_groups(groups, count):
-    for key in groups:
-        if len(groups[key].item_tuples) == count:
-            compiled = []
-            for tup in groups[key].item_tuples:
-                compiled.append("%s %s" % tup)
-            print(', '.join(compiled))
+def print_groups(groups):
+    for key, group in groups.iteritems():
+        if len(group.item_tuples) == 10:
+            print(', '.join(["%s %s" % (tuple(tup)) for tup in group.item_tuples]))
 
 def find_anagram(word_list):
     combined_words = itertools.combinations(word_list, 2)
@@ -34,10 +30,11 @@ def find_anagram(word_list):
 
     for word_one, word_two in combined_words:
         joined = ''.join(sorted("%s%s" % (word_one, word_two)))
-        if not unique_groups.get(joined):
-            unique_groups[joined] = UniqueThing()
-        unique_groups[joined].add_tuple(word_one, word_two)
-
+        unique = unique_groups.get(joined)
+        if not unique:
+            unique = UniqueThing()
+            unique_groups[joined] = unique
+        unique.add_tuple(word_one, word_two)
     return unique_groups
 
 if __name__ == '__main__':
@@ -47,5 +44,5 @@ if __name__ == '__main__':
     print(time.time() - start_time, "seconds")
     groups = find_anagram(word_list)
     print(time.time() - start_time, "seconds")
-    print_groups(groups, 10)
+    print_groups(groups)
     print(time.time() - start_time, "seconds")

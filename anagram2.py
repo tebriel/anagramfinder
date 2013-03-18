2432c83a955679a5c509090bf32689b869e881d3
Chris Moultrie <cmoultrie@endgames.us>
2013-03-17 21:36:09 -0700

    More changes

--- anagram2.py
+++ anagram2.py
@@ -3,11 +3,18 @@ import itertools
 import sys
 import gc
 
-def print_groups(groups):
-    for key in groups:
-        group = groups[key]
-        if len(group) == 20:
+def filter_groups(groups):
+    for key, group in groups.iteritems():
+        if len(group) < 20: continue
+        newGroup = []
+        for idx in range(0, len(group), 2):
+            word_one = group[idx]
+            word_two = group[idx + 1]
+            if word_one not in newGroup and word_two not in newGroup:
+                newGroup.extend([word_one, word_two])
+        if len(newGroup) == 20:
             print(', '.join(["%s %s" % (group[idx], group[idx + 1]) for idx in range(0, 20, 2)]))
+            
 
 def find_anagram(word_list):
     unique_groups = {}
@@ -15,8 +22,7 @@ def find_anagram(word_list):
     for word_one, word_two in itertools.combinations(word_list, 2):
         joined = ''.join(sorted("%s%s" % (word_one, word_two)))
         unique = unique_groups.setdefault(joined, list())
-        if not word_one in unique and not word_two in unique:
-            unique.extend([word_one, word_two])
+        unique.extend([word_one, word_two])
 
     return unique_groups
 
@@ -25,4 +31,4 @@ if __name__ == '__main__':
     text = sys.stdin.read()
     word_list = set(re.findall('[a-z]{4,}', text.lower()))
     groups = find_anagram(word_list)
-    print_groups(groups)
+    filter_groups(groups)

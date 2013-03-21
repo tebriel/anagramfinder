bcf85caa566e511f8fa7b03771e09f943ac7cadc
Chris Moultrie <cmoultrie@endgames.us>
2013-03-21 16:58:04 -0400

    Updated to be faster

--- anagram2.py
+++ anagram2.py
@@ -4,25 +4,29 @@ import sys
 import gc
 
 def filter_groups(groups):
-    for group in groups.itervalues():
-        if len(group) < 20: continue
+    _range = range
+    _len = len
+    for group in groups.values():
+        if _len(group) < 20: continue
         newGroup = []
-        for idx in range(0, len(group), 2):
+        for idx in _range(0, _len(group), 2):
             word_one = group[idx]
             word_two = group[idx + 1]
             if word_one not in newGroup and word_two not in newGroup:
                 newGroup.extend([word_one, word_two])
-        if len(newGroup) == 20:
-            print(', '.join(["%s %s" % (group[idx], group[idx + 1]) for idx in range(0, 20, 2)]))
+        if _len(newGroup) == 20:
+            print(', '.join(["%s %s" % (group[idx], group[idx + 1]) for idx in _range(0, 20, 2)]))
             
 
 def find_anagram(word_list):
+    _sorted = sorted
+    _list = list
     unique_groups = {}
+    combi = itertools.combinations(word_list, 2)
 
-    for word_one, word_two in itertools.combinations(word_list, 2):
-        joined = ''.join(sorted("%s%s" % (word_one, word_two)))
-        unique = unique_groups.setdefault(joined, [])
-        unique.extend([word_one, word_two])
+
+    for pair in combi:
+        unique_groups.setdefault(''.join(_sorted("%s%s" % pair)), []).extend(pair)
 
     return unique_groups
 
